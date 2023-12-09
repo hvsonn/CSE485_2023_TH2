fa665290bd002c431c158f0283223850279c4028
thuongpower <142953480+thuongpower@users.noreply.github.com>
2023-12-09 17:31:02 +0700

    update db

--- db/btth02_1.sql
+++ db/btth02_1.sql
@@ -45,7 +45,21 @@ CREATE TABLE `comment` (
 ALTER TABLE `comment`
   ADD PRIMARY KEY (`id`);
 COMMIT;
-
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (1, 1, 'Great job!', 'Major Blakeley', '2023-03-19');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (2, 2, 'Well done!', 'Yance Prinett', '2022-10-07');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (3, 3, 'Great job!', 'Jamaal Cressingham', '2023-04-10');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (4, 4, 'Impressive!', 'Manya Lyttle', '2023-09-19');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (5, 5, 'Impressive!', 'Iain MacArte', '2023-01-01');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (6, 6, 'Fantastic!', 'Silvester Sabberton', '2023-02-25');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (7, 7, 'Keep it up!', 'Augustine Abramof', '2023-08-15');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (8, 8, 'Impressive!', 'Denna Varsey', '2023-10-02');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (9, 9, 'Bravo!', 'Bathsheba Witz', '2022-05-26');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (10, 10, 'I love it!', 'Alidia Trevon', '2022-05-27');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (11, 11, 'Well done!', 'Stephan Hencke', '2023-12-03');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (12, 12, 'Great job!', 'Hobart Galland', '2022-10-20');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (13, 13, 'Amazing work!', 'Darby Scrogges', '2023-04-30');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (14, 14, 'Impressive!', 'Berne Floris', '2023-10-01');
+insert into MOCK_DATA (id, parent_id, comment, sender, date) values (15, 15, 'I love it!', 'Crin Pacher', '2023-11-03');
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- db/cms.sql
+++ db/cms.sql
@@ -31,7 +31,21 @@ CREATE TABLE `cms_category` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+insert into cms_category (id, name) values (1, 'Simona Hornig');
+insert into cms_category (id, name) values (2, 'Alfonse Braikenridge');
+insert into cms_category (id, name) values (3, 'Xavier Lankester');
+insert into cms_category (id, name) values (4, 'Farley Bowland');
+insert into cms_category (id, name) values (5, 'Packston Surtees');
+insert into cms_category (id, name) values (6, 'Judd Le febre');
+insert into cms_category (id, name) values (7, 'Krystle Perutto');
+insert into cms_category (id, name) values (8, 'Janeva Botly');
+insert into cms_category (id, name) values (9, 'Dacie Trighton');
+insert into cms_category (id, name) values (10, 'Cordie Largen');
+insert into cms_category (id, name) values (11, 'Ashbey Plaistowe');
+insert into cms_category (id, name) values (12, 'Tallulah Allcroft');
+insert into cms_category (id, name) values (13, 'Wilfrid McNae');
+insert into cms_category (id, name) values (14, 'Marcia Esseby');
+insert into cms_category (id, name) values (15, 'Meghan Spaunton');
 -- --------------------------------------------------------
 
 --
@@ -48,7 +62,21 @@ CREATE TABLE `cms_posts` (
   `created` datetime NOT NULL,
   `updated` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+insert into cms_category (id, title, message, category_id, userid, status, created) values (1, 'Staff Accountant IV', 'Butorides striatus', 1, 1, 'draft', '2023-04-27');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (2, 'Quality Control Specialist', 'Alectura lathami', 2, 2, 'archived', '2023-07-21');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (3, 'Pharmacist', 'Ammospermophilus nelsoni', 3, 3, 'published', '2022-10-10');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (4, 'Payment Adjustment Coordinator', 'Dolichitus patagonum', 4, 4, 'draft', '2023-04-25');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (5, 'Senior Developer', 'Eumetopias jubatus', 5, 5, 'archived', '2022-08-05');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (6, 'Nurse', 'Echimys chrysurus', 6, 6, 'published', '2023-02-01');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (7, 'Graphic Designer', 'Neophron percnopterus', 7, 7, 'archived', '2022-11-02');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (8, 'Software Test Engineer I', 'Calyptorhynchus magnificus', 8, 8, 'archived', '2023-06-29');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (9, 'Community Outreach Specialist', 'Vombatus ursinus', 9, 9, 'archived', '2023-11-21');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (10, 'Senior Developer', 'Mycteria leucocephala', 10, 10, 'published', '2022-12-08');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (11, 'Research Associate', 'Anthropoides paradisea', 11, 11, 'draft', '2023-05-17');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (12, 'Community Outreach Specialist', 'Bettongia penicillata', 12, 12, 'published', '2022-12-29');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (13, 'Automation Specialist III', 'Mazama gouazoubira', 13, 13, 'published', '2023-07-21');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (14, 'Programmer Analyst IV', 'Arctogalidia trivirgata', 14, 14, 'archived', '2023-02-28');
+insert into cms_category (id, title, message, category_id, userid, status, created) values (15, 'Legal Assistant', 'Pseudocheirus peregrinus', 15, 15, 'draft', '2022-06-29');
 -- --------------------------------------------------------
 
 --
@@ -64,7 +92,21 @@ CREATE TABLE `cms_user` (
   `type` int(11) NOT NULL,
   `deleted` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (1, 'Simona', 'Hornig', 'mjentin0@t.co', 'kH__c4m/72dG2', 1,1);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (2, 'Alfonse', 'Braikenridge', 'psellor1@edublogs.org', 'rDI/Ces{}%xK{', 0, 2);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (3, 'Xavier', 'Lankester', 'jhillatt2@last.fm', 'sV#>6?Guy"zhcoJ', 0, 3);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (4, 'Farley', 'Bowland', 'lmccaughan3@narod.ru', 'bW~OpXnG',0, 4);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (5, 'Packston', 'Surtees', 'lcorryer4@mlb.com', 'wUSoQ!o8=QOmAL$', 0, 5);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (6, 'Judd', 'Le febre', 'dpresnall5@flickr.com', 'zV''?}ujfA<J', 0, 6);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (7, 'Krystle', 'Perutto', 'mgibby6@princeton.edu', 'uVPY|@Qj0dP', 0, 7);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (8, 'Janeva', 'Botly', 'ralderwick7@naver.com', 'nD>dCKQso}e4kB', 0, 8);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (9, 'Dacie', 'Trighton', 'sgavozzi8@cisco.com', 'yD|1wiHI?Z', 0, 9);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (10, 'Cordie', 'Reston', 'sreston9@gizmodo.com', 'dTr{m\3~u,5<t', 0, 10);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (11, 'Ashbey', 'Plaistowe', 'wwashingtona@tinypic.com', 'zP$YdSLtrc', 0, 11);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (12, 'Tallulah', 'Allcroft', 'rgallonb@admin.ch', 'fXpc(A3iJa1w<&&V', 0, 12);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (13, 'Wilfrid', 'McNae', 'tstrelitzerc@alexa.com', 'aA0`/dN/MG', 0, 13);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (14, 'Marcia', 'Esseby', 'enorthleyd@over-blog.com', 'fRV=Mr++3', 0, 14);
+insert into cms_user (id, first_name, last_name, email, password, type, deleted) values (15, 'Meghan', 'Spaunton', 'dslimingse@theatlantic.com', 'bV*sM*%ycE2sn6hb', 0, 15);
 --
 -- Chỉ mục cho các bảng đã đổ
 --
