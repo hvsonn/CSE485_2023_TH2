fef68229cadddacabd631f0b2b443478a4dfe59a
thietlang <110318687+thietlang@users.noreply.github.com>
2023-12-09 18:09:01 +0700

    update fontend

---
+++ admin/menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/Index.css">
+  <title>Left Menu and Tables</title>
+</head>
+<body>
+  <aside>
+    <h2>Menu</h2>
+    <ul class="mucluc">
+      <li><a href="#">Dashboard</a></li>
+      <li><a href="#">Categories</a></li>
+      <li><a href="#">Posts</a></li>
+      <li><a href="#">Users</a></li>
+    </ul>
+  </aside>
+
+
+</body>
+</html>
